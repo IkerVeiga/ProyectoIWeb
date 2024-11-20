5eeb54005cb6d8cdede63e051b97b80cfd83984c
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-11-20 17:04:20 +0100

    Er javascript

--- ProyectoIngenieriaWeb/ApertureScience/static/scripts/prueba.js
+++ ProyectoIngenieriaWeb/ApertureScience/static/scripts/prueba.js
@@ -1,1 +1,26 @@
-alert("Hola");
+const pricePrefix = "Precio: ";
+
+const API = {
+    key : "fca_live_BYOK83s2qtf3Lf99SGERrqdon4AlnsNaq9b7wF76",
+    serviceUrl : "https://api.freecurrencyapi.com/v1/latest"
+}
+
+function convert(quantity, toDivise) {
+    let url = API.serviceUrl + "?apikey=" + API.key + "&currencies="+toDivise;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(json => {
+        let data = json.data;
+        let divise = Object.keys(data)[0];
+        let convertedPrice = quantity * data[divise];
+        precioDiv.innerHTML = pricePrefix + convertedPrice + " " + divise;
+    });
+}
+
+let precioDiv = document.getElementById("precio");
+let precio = parseInt(precioDiv.innerHTML);
+
+//Modificar el precio
+console.log(precio);
+convert(precio, "EUR");
+
+precioDiv.innerHTML = pricePrefix + precio;
