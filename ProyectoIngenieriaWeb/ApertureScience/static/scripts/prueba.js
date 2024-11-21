49e5da6d8492212bf559d687811d1d6027e0e72a
GaizkaCarrasco <gaizka.carrasco@opendeusto.es>
2024-11-21 08:59:43 +0100

    cambio monedas con botón

--- ProyectoIngenieriaWeb/ApertureScience/static/scripts/prueba.js
+++ ProyectoIngenieriaWeb/ApertureScience/static/scripts/prueba.js
@@ -20,7 +20,17 @@ let precioDiv = document.getElementById("precio");
 let precio = parseInt(precioDiv.innerHTML);
 
 //Modificar el precio
-console.log(precio);
-convert(precio, "EUR");
+document.addEventListener('DOMContentLoaded', () => {
+    var selectElement = document.getElementById('coin');
+    
+    selectElement.addEventListener('change', function() {
+        var selectedValue = selectElement.value;
+        console.log('Selected currency:', selectedValue);
+        // Aquí puedes hacer lo que necesites con el valor seleccionado
+        console.log(precio);
+        convert(precio, selectedValue);
+    });
+});
 
-precioDiv.innerHTML = pricePrefix + precio;
+
+precioDiv.innerHTML = pricePrefix + precio + " USD";
