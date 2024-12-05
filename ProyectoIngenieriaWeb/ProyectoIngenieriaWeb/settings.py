cac635b5fc1bc09b0cb69c1be6692808b871bbde
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-12-05 09:28:40 +0100

    Usuarios y grupos

--- ProyectoIngenieriaWeb/ProyectoIngenieriaWeb/settings.py
+++ ProyectoIngenieriaWeb/ProyectoIngenieriaWeb/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ApertureScience\static')]
+
+AUTH_PASSWORD_VALIDATORS = []
